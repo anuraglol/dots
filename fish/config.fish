6b4e76158e87df28d31501123b99317c86743869
Anurag <kr.anurag24@gmail.com>
2026-03-24 00:40:58 +0530

    init

---
+++ fish/config.fish
@@ -0,0 +1,16 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+function fish_greeting
+end
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+
+zoxide init fish | source
+alias cd 'z'
+
+# opencode
+fish_add_path /home/anurag/.opencode/bin
